d0fd69ae5d24b4c6d04b965e345abacf3cf5b100
Patihan Prasongphol <patihan70@gmail.com>
2025-09-23 21:53:38 +0700

    fix(eslint): remove require() imports from test setup
    - Remove ESLint errors by eliminating require() statements
    - Simplify test setup - happy-dom provides necessary Web APIs automatically
    - Remove unnecessary polyfills that were causing TypeScript type conflicts
    - All CI pipeline steps now pass locally (lint, format, test, build)

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,16 +1,4 @@
 import '@testing-library/jest-dom';
 
-// Essential polyfills for Node.js test environment
-if (typeof global !== 'undefined') {
-  // TextEncoder/TextDecoder for jsdom
-  if (!global.TextEncoder) {
-    const { TextEncoder, TextDecoder } = require('util');
-    global.TextEncoder = TextEncoder;
-    global.TextDecoder = TextDecoder;
-  }
-  
-  // URL constructor for Node.js
-  if (!global.URL) {
-    global.URL = require('url').URL;
-  }
-}
+// Note: happy-dom environment should provide most Web APIs automatically
+// Only add polyfills if tests fail without them
